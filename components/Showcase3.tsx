30b7682e3ad21842d2b2755e5681c35389a6ef50
Mehdi Zibout <mehdizibout@gmail.com>
2023-02-05 19:40:02 +0100

    Fix showcase3 mobile

--- components/Showcase3.tsx
+++ components/Showcase3.tsx
@@ -9,7 +9,7 @@ function Showcase3() {
       <div
         className={`rounded-lg overflow-hidden bg-no-repeat bg-cover bg-[url(/assets/home/mobile/image-earphones-yx1.jpg)] tablet:bg-[url(/assets/home/tablet/image-earphones-yx1.jpg)] desktop:bg-[url(/assets/home/desktop/image-earphones-yx1.jpg)] tablet:w-[50%] h-[200px] tablet:h-[320px]`}
       ></div>
-      <div className="bg-[#F1F1F1] rounded-lg px-6 py-10 tablet:px-10 desktop:px-[95px] mt-6 tablet:mt-0 ml-3 tablet:w-[50%] tablet:flex tablet:flex-col tablet:justify-center">
+      <div className="bg-[#F1F1F1] rounded-lg px-6 py-10 tablet:px-10 desktop:px-[95px] mt-6 tablet:mt-0 tablet:ml-3 tablet:w-[50%] tablet:flex tablet:flex-col tablet:justify-center">
         <h2 className="text-[1.75rem] tracking-[2px] mb-8 ">YX1 EARPHONES</h2>
         <button className="btn btn-secondary font-bold w-fit">
           see product
